8a55fbc9d11fec52c6f8d0d3e35c0c11c87e5945
kajalthakur99 <kajal.01@dbalounge.com>
2024-04-25 15:09:18 +0530

    changing port

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URL: "http://144.24.96.140:8070/"
+        BASE_URL: "http://144.24.96.140:8070"
     }
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig

4e38d898e59616cfb5d3e9fd6a30fafe78150b3e
manish sharma <manish.sharma@dbalounge.com>
2024-04-26 13:50:37 +0530

    set card on page kyccount and updatecoun

--- app/uploadfile/page.js
+++ app/uploadfile/page.js
@@ -14,48 +14,67 @@ const UploadPage = () => {
     },[])
 
     return (
-        <div className="container-fluid">
-        <div className="m-3">
+        <div className="container-fluid" style={{ height: '100vh', overflowY: 'auto' }}>
+            <div className="m-3">
                 <div className="d-flex justify-content-end mb-2">
                     <button className="btn btn-outline-danger" onClick={handleLogout}>
                         Logout
                     </button>
                 </div>
-            <div className="container mt-3">
-                <Branding />
-               <b><p>ExistingKyc: {kyccount.existingKyc}</p></b>
-               <b> <p>UpdatedKyc: {kyccount.updatedKyc}</p></b>
-                <form className="row align-items-end mt-2" onSubmit={invokeUploadFileSubmitHandler}>
-                    <div className="col-md-10">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <label>Upload File</label>
-                                <input
-                                    type="file"
-                                    name="file"
-                                    className="form-control"
-                                    accept=".xlsx"
-                                    required
-                                    onChange={uploadFileChangeHandler}
-                                />
+                <div className="container mt-3">
+                    <Branding />
+                    <form className="row align-items-end mt-2" onSubmit={invokeUploadFileSubmitHandler}>
+                        <div className="col-md-10">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <label>Upload File</label>
+                                    <input
+                                        type="file"
+                                        name="file"
+                                        className="form-control"
+                                        accept=".xlsx"
+                                        required
+                                        onChange={uploadFileChangeHandler}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <button className="btn btn-primary" type="submit">
+                                Upload
+                            </button>
+                        </div>
+                    </form>
+                    <div className="errorcontainer success-container">
+                        {uploadFile?.error && <p className="error-message">{uploadFile?.error}</p>}
+                        {uploadFile?.success && <p className="success-container">{uploadFile?.success}</p>}
+                    </div>
+
+                </div>
+                    <div className="row mt-3 file_container">
+                        <div className="col-md-2">
+                            <div className="card">
+                                <div className="card-body">
+                                <img className='kyccount' src="/image.png" alt="file_logo" />
+                                    <b><p className='card-title kyccount1 '>{kyccount.existingKyc}</p></b>
+                                    <b><p className='card-title kyccount1'>ExistingKyc</p></b>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="card">
+                                <div className="card-body ">
+                                <img className='updatedKyc' src="/image.png" alt="file_logo" />
+                                    <b><p className='card-title'>{kyccount.updatedKyc}</p></b>
+                                    <b><p className='card-title'>UpdatedKyc</p></b>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-2">
-                        <button className="btn btn-primary" type="submit">
-                            Upload
-                        </button>
-                    </div>
-                </form>
-                <div className="errorcontainer success-container">
-                    {uploadFile?.error && <p className="error-message">{uploadFile?.error}</p>}
-                    {uploadFile?.success && <p className="success-container">{uploadFile?.success}</p>}
-                </div>
             </div>
+            <Footer />
         </div>
-        <Footer />
-    </div>
-);
+    );
 };
 
 export default UploadPage;

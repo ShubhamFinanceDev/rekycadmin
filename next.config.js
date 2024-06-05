35a9e34fbd52cc574a2038560a1ae2afaabc2035
nainish <pmo_dbalounge@shubham.co>
2024-06-05 23:39:40 +0530

    prod url update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URL: "http://144.24.96.140:8070"
+        BASE_URL: "http://10.16.1.131:8070"  //prod url
         
     }
 }

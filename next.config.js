4a518c1759a23ec803153b0cda1ec760f47b991e
nainish <pmo_dbalounge@shubham.co>
2024-06-06 16:28:14 +0530

    publuic url update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-            BASE_URL:"https//152.67.24.187:8443" 
+            BASE_URL:"https://152.67.24.187:8443" 
     }
 }
 

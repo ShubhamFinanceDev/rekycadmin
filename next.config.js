2ea84b34445f3df8dd9b1c45e6285b6541fd9a79
kajalthakur99 <kajal.01@dbalounge.com>
2024-06-06 14:49:09 +0530

    adding url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URL: "http://10.16.1.46:8070"
-
+        // BASE_URL: "http://10.16.1.46:8070"
+            BASE_URL:"https//152.67.24.187:8443"
         // BASE_URL: "http://10.16.1.131:8070"  //prod url
 
         

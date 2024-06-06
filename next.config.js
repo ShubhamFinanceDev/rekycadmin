d2b9d3bd53355677625afd299bbadc0aba26f095
nainish <pmo_dbalounge@shubham.co>
2024-06-06 15:23:07 +0530

    public ip update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // BASE_URL: "http://10.16.1.46:8070"
-            BASE_URL:"https//152.67.24.187:8443"
-        // BASE_URL: "http://10.16.1.131:8070"  //prod url
-
-        
+            BASE_URL:"https//152.67.24.187:8443" 
     }
 }
 

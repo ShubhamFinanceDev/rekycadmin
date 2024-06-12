37d31eb48c21a53ba2ccfa0c08a6696f88b0cd86
manish sharma <manish.sharma@dbalounge.com>
2024-06-12 17:35:17 +0530

    config set

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
+
             // BASE_URL:"https://152.67.24.187:8443" 
-            BASE_URL:"http://144.24.96.140:8070" 
+
+        // BASE_URL: "http://144.24.96.140:8070"
+         BASE_URL:"https://Rekyc.shubham.co:8443"
+    
     }
 }
 

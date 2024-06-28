c1ca4e324b4bc1877efdfc98c30618f293d83b2c
saurabh17kumar <kumar.saurabh@dbalounge.com>
2024-06-28 12:47:19 +0530

    uid pass

--- services/endpoint.js
+++ services/endpoint.js
@@ -5,7 +5,7 @@ const API = {
     invokekyc: (Uid= "") => `admin/invoke-kyc-process-flag?uid=${Uid}`,
     validEmailPassword: () => `admin/login`,
     kyccount: (Uid= "") => `/admin/kycCount?uid=${Uid}`,
-    genratereport: () => `/admin/generate-report`
+    genratereport: (Uid= "") => `/admin/generate-report?uid=${Uid}`
 
 }
 

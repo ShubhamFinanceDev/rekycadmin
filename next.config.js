7364d91276edbdeb82c550d4254c97f09756e359
kajalthakur99 <155451984+kajalthakur99@users.noreply.github.com>
2024-07-03 14:22:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 	/** @type {import('next').NextConfig} */
     const nextConfig = {
         env: {
-            BASE_URL: "http://144.24.96.140:8070"
+            BASE_URL: "https://Rekyc.shubham.co:8443"
             
         }
     }
